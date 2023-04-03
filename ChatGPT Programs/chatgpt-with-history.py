ab534da089589ddcc4a498d58367ef37eaf314ec
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2023-04-03 19:18:40 +0100

    Update chatgpt-with-history.py

--- ChatGPT Programs/chatgpt-with-history.py
+++ ChatGPT Programs/chatgpt-with-history.py
@@ -1,1 +1,35 @@
+import openai
 
+def main():
+
+    openai.api_key = "ENTER YOUR API KEY HERE"
+
+    user_name = input("Enter your name: ")
+
+    behavior = input(f"Hi {user_name}, enter the style you want ChatGPT to behave in: ")
+
+    user_question = input(f"{user_name}: ")
+
+    answer = openai.ChatCompletion.create(
+
+        model = "gpt-3.5-turbo",
+
+        messages = [
+            {"role": "system", "content": behavior},
+            {"role": "assistant", "content": user_question}
+        ]
+    )
+
+    message_body = answer.choices[0].message.content
+
+    print(f"ChatGPT: {message_body}")
+
+while True:
+
+    main()
+
+    repeat = input("Do you want to ask another quesation to ChatGPT (y/n)?")
+
+    if repeat != "Y" or repeat != "y":
+
+        break
